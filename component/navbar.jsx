4621d4a405f22282aacd84c738be90894f6b0841
AshkanTavangar <ashkantavangar7@gmail.com>
2022-07-26 16:26:15 +0430

    use user's page in navbar

--- component/navbar.jsx
+++ component/navbar.jsx
@@ -2,10 +2,12 @@ import navStyle from '../styles/Navbar.module.css';
 import Link from "next/link";
 
 const Navbar = ()=>{
+    const username = "james";
     return(
         <header className={navStyle.navbar}>
             <nav>
                 <Link  href='/'><a className={navStyle.link}>Home</a></Link>
+                <Link  href={`/user/${username}`}><a className={navStyle.link}>user</a></Link>
                 <Link  href='/about'><a className={navStyle.link}>About</a></Link>
             </nav>
         </header>
